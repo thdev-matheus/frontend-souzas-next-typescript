e9ec6e39ddb2ffdf710a79b816ab1d1df00288a3
Theus <matheusth.dev@gmail.com>
2023-10-14 23:51:16 -0300

    feat: coleta de dados dos inputs do formulário de login

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -4,11 +4,15 @@ import { ReactNode } from "react";
 
 import { Providers } from "@/providers";
 import { Styles } from "@/styles";
+import { Toaster } from "react-hot-toast";
 
 export const Config = ({ children }: { children: ReactNode }) => {
   return (
-    <Providers>
-      <Styles>{children}</Styles>
-    </Providers>
+    <>
+      <Providers>
+        <Styles>{children}</Styles>
+      </Providers>
+      <Toaster />
+    </>
   );
 };
--- src/blocks/LoginFormBlock/index.tsx
+++ src/blocks/LoginFormBlock/index.tsx
@@ -3,14 +3,50 @@
 import * as S from "./styles";
 import * as C from "@/components";
 
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+interface IUserLogin {
+  username: string;
+  password: string;
+}
+
 export const LoginFormBlock = () => {
+  const { reset, handleSubmit, register } = useForm<IUserLogin>({
+    reValidateMode: "onSubmit",
+  });
+
+  const handleLogin = (data: IUserLogin) => {
+    if (!data.username) {
+      toast.error("Digite o usuário");
+    }
+    if (!data.password) {
+      toast.error("Digite a senha");
+    }
+
+    if (!data.username || !data.password) {
+      return;
+    }
+
+    console.log(data);
+    reset();
+  };
+
   return (
     <S.Container>
       <h2>Faça Seu Login</h2>
 
-      <S.Form>
-        <C.Input placeholder="Digite seu usuário" borderRadius=".5rem" />
-        <C.Input placeholder="Digite sua senha" borderRadius=".5rem" />
+      <S.Form onSubmit={handleSubmit(handleLogin)}>
+        <C.Input
+          placeholder="Digite seu usuário"
+          borderRadius=".5rem"
+          {...register("username")}
+        />
+        <C.Input
+          placeholder="Digite sua senha"
+          borderRadius=".5rem"
+          {...register("password")}
+        />
 
         <S.BoxButton>
           <C.Button label="Entrar" onAction={() => {}} />
