39084adc01c69f7e279101e77f5b4df48871ff04
Theus <matheusth.dev@gmail.com>
2023-10-17 01:08:02 -0300

    feat: responsividade da página inicial

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -10,17 +10,6 @@ export const Container = styled.section`
   justify-content: space-between;
 
   padding: 4rem 0;
-  @media (min-width: 481px) {
-    width: 100vw;
-    height: 100vh;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-
-    padding: 4rem 0;
-  }
 `;
 
 export const BoxLoading = styled.div`
@@ -30,21 +19,9 @@ export const BoxLoading = styled.div`
   justify-content: center;
   gap: 1.5rem;
   text-align: center;
-  @media (min-width: 481px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1.5rem;
-    text-align: center;
-  }
 `;
 
 export const BoxButton = styled.div`
   width: 12rem;
   height: 2.5rem;
-  @media (min-width: 481px) {
-    width: 12rem;
-    height: 2.5rem;
-  }
 `;
--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -21,26 +21,4 @@ export const Container = styled.section`
 
     color: ${(props) => props.theme.secondary};
   }
-  @media (min-width: 481px) {
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    text-align: center;
-
-    & > h1 {
-      font-size: 40pt;
-    }
-
-    & > h2 {
-      font-size: 20pt;
-      font-weight: lighter;
-
-      color: ${(props) => props.theme.secondary};
-    }
-  }
 `;
