1aa2d04f98cdab7a276f274a041e6dab0e9506e3
Theus <matheusth.dev@gmail.com>
2023-10-17 22:51:35 -0300

    feat: responsividade do bloco de informações

--- src/components/UserCard/styles.ts
+++ src/components/UserCard/styles.ts
@@ -1,52 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  @media (min-width: 481px) {
-  }
+  width: 100%;
+  height: 3.5rem;
 
-  @media (min-width: 768px) {
-  }
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1rem;
 
-  @media (min-width: 992px) {
-  }
+  padding: 0 1rem;
 
-  @media (min-width: 1200px) {
-    width: 100%;
-    height: 3.5rem;
+  border-radius: 0.5rem;
+
+  background-color: ${(props) => props.theme.bgTertiary};
+
+  & > .box_img {
+    width: 2.5rem;
+    height: 2.5rem;
 
     display: flex;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
-    gap: 1rem;
 
-    padding: 0 1rem;
+    border-radius: 50%;
 
-    border-radius: 0.5rem;
+    overflow: hidden;
 
-    background-color: ${(props) => props.theme.bgTertiary};
-
-    & > .box_img {
-      width: 2.5rem;
-      height: 2.5rem;
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      border-radius: 50%;
-
-      overflow: hidden;
-
-      & > img {
-        width: 100%;
-      }
-    }
-
-    & > h4 {
-      font-size: 10pt;
+    & > img {
+      width: 100%;
     }
   }
 
-  @media (min-width: 1920px) {
+  & > h4 {
+    font-size: 10pt;
   }
 `;
